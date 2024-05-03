c158589ff53deb09278ec51e76c8c57d3fd741df
Aryan Agrahari <57763333+aryan757@users.noreply.github.com>
2024-05-03 09:13:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -82,5 +82,4 @@ def index():
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001, debug=True)
 	app.run(debug=True)
-#this is updated 
-# this is my second updates 
+#main.py file has been updated.
